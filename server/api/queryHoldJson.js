d4dfe631a8368c6d00fc09ea41cad2da5e66b03c
wanghuan <1032290763@QQ.COM>
2025-09-24 20:31:06 +0800

    1

--- server/api/queryHoldJson.js
+++ server/api/queryHoldJson.js
@@ -17,6 +17,7 @@ export async function get_持仓JSON() {
 export default eventHandler(async (event) => {
   // return MOCK_HOLD;
   const 持仓JSON = await get_持仓JSON();
+  if (!持仓JSON?.length) return MOCK_HOLD;
   // const all_data = MOCK_DATA;
   return 持仓JSON;
 });
