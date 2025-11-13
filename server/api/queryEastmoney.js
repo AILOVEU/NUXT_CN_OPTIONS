7a28a85f76794717a3c72a1f9ab471b39c29ac9f
wanghuan <1032290763@QQ.COM>
2025-11-13 10:16:25 +0800

    1111

--- server/api/queryEastmoney.js
+++ server/api/queryEastmoney.js
@@ -8,8 +8,8 @@ export default eventHandler(async (event) => {
       referer: "https://quote.eastmoney.com/center/gridlist.html",
       "user-agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36 Edg/141.0.0.0",
-      cookie:
-        "fullscreengg=1; fullscreengg2=1; st_nvi=J-zxfU2hGTSaYot7gcCmD7800; qgqp_b_id=9b5c5b018a8a9ff2f8d9780997fc2519; st_si=20502849844047; st_pvi=21140416938263; st_sp=2024-12-06%2009%3A08%3A33; st_inirUrl=https%3A%2F%2Fwww.bing.com%2F; st_sn=1; st_psi=20251031100312431-113200301321-3759329563; st_asi=delete",
+      cookie: 
+        "qgqp_b_id=de87bbec62f1d2c7d9626e3127ebb846; st_nvi=9sfcXbsJWFgqX6J9wo5098db5; nid=0c1248703dd9b67ef59239379fc2e655; nid_create_time=1762493884180; gvi=eBoch3lM2OTpe8jqlrooI9f18; gvi_create_time=1762493884181; websitepoptg_api_time=1762939113305; st_si=23799796147610; st_asi=delete; fullscreengg=1; fullscreengg2=1; st_pvi=56990777245722; st_sp=2024-01-02%2010%3A52%3A25; st_inirUrl=https%3A%2F%2Fwww.google.com%2F; st_sn=5; st_psi=2025111310122581-113200301321-7358958801",
     },
   });
   return res;
