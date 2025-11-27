075ec21fa8afc0edda354b5a8635d7dba7b5a1d5
wanghuan <1032290763@QQ.COM>
2025-11-27 18:07:02 +0800

    6666

--- server/api/queryEastmoney.js
+++ server/api/queryEastmoney.js
@@ -3,8 +3,7 @@ import { generateEastMoneyCookies } from "./cookieClass";
 export default eventHandler(async (event) => {
   let st_sp = encodeURIComponent(dayjs().format("YYYY-MM-DD HH:mm:ss"));
   let st_psi = `${dayjs().format("YYYYMMDDHHmmssSSS")}-113200301321-9808443140`;
-  let cookie = `st_si=37609242318303; st_asi=delete; st_pvi=73366267206983; st_sp=${st_sp}; st_inirUrl=https%3A%2F%2Foption.eastmoney.com%2F; st_sn=2; st_psi=${st_psi}`;
-  cookie = `fullscreengg=1; fullscreengg2=1; st_nvi=nowKK43zqBo9z2DA3YqiHd1dd; qgqp_b_id=9b5c5b018a8a9ff2f8d9780997fc2519; st_si=52023999008088; st_pvi=60507986574562; st_sp=2025-11-26%2015%3A33%3A41; st_inirUrl=; st_sn=1; st_psi=20251126153341212-113200301321-7054887615; st_asi=delete`
+  let cookie = `st_nvi=nowKK43zqBo9z2DA3YqiHd1dd; qgqp_b_id=9b5c5b018a8a9ff2f8d9780997fc2519; nid=010d039dd427dc4d187090491f47d7ad; nid_create_time=1764142421507; gvi=iXLX2FIhQ5XluzRbcENiJ6e15; gvi_create_time=1764142421507; fullscreengg=1; fullscreengg2=1; st_si=62478325838925; st_pvi=60507986574562; st_sp=2025-11-26%2015%3A33%3A41; st_inirUrl=; st_sn=1; st_psi=20251127175944589-113200301321-3443841300; st_asi=delete`
   // 生成初始cookie
   // cookie = generateEastMoneyCookies();
   console.log(cookie)
