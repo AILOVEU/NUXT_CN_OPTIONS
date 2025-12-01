3daae9bfe86782d60c62883e981a3021b0b4fd1b
wanghuan <1032290763@QQ.COM>
2025-12-01 15:10:30 +0800

    7777

--- server/api/queryEastmoney.js
+++ server/api/queryEastmoney.js
@@ -1,8 +1,6 @@
 import dayjs from "dayjs";
 export default eventHandler(async (event) => {
-  let st_sp = encodeURIComponent(dayjs().format("YYYY-MM-DD HH:mm:ss"));
-  let st_psi = `${dayjs().format("YYYYMMDDHHmmssSSS")}-113200301321-9808443140`;
-  let cookie = `st_nvi=nowKK43zqBo9z2DA3YqiHd1dd; qgqp_b_id=9b5c5b018a8a9ff2f8d9780997fc2519; nid=010d039dd427dc4d187090491f47d7ad; nid_create_time=1764142421507; gvi=iXLX2FIhQ5XluzRbcENiJ6e15; gvi_create_time=1764142421507; fullscreengg=1; fullscreengg2=1; st_si=62478325838925; st_pvi=60507986574562; st_sp=2025-11-26%2015%3A33%3A41; st_inirUrl=; st_sn=1; st_psi=20251127175944589-113200301321-3443841300; st_asi=delete`
+  let cookie = `fullscreengg=1; fullscreengg2=1; qgqp_b_id=9b5c5b018a8a9ff2f8d9780997fc2519; st_nvi=85waxIoBYEnN5gxMGZI2a5727; st_si=56304214774557; st_asi=delete; nid18=010d039dd427dc4d187090491f47d7ad; nid18_create_time=1764572853092; gviem=-QpcsDjocpPdoWOXH3f_e7a18; gviem_create_time=1764572853093; st_pvi=54843293934490; st_sp=2025-12-01%2015%3A07%3A32; st_inirUrl=; st_sn=2; st_psi=20251201150809369-113200301321-3043073337`
   const res = await $fetch("https://push2.eastmoney.com/api/qt/clist/get", {
     method: "get",
     params: getQuery(event),
--- server/api/querySaveData.js
+++ server/api/querySaveData.js
@@ -6,10 +6,7 @@ export default eventHandler(async (event) => {
   const body = await readBody(event);
   const jsonObj = body.data;
   const parse = new Parser();
-  // let content = parse.parse(jsonObj);
-  let content = `"f2","f14","f18","f31","f32","f108","f161","f163","f249","f250","f301","f303","f325","f326","f328","f333","f334"
-  0.1705,"50ETF购12月2950",0.1673,0.1685,0.1718,8749,2.95,690,14.65,0.24,20251224,17.05,0.9341,1.1196,-0.1348,"上证50ETF",3.113
-  0.2988,"50ETF购3月2850",0.2988,0.2882,0.2968,827,2.85,0,16.48,1.15,20260325,9.3,0.8927,0.7715,-0.1007,"上证50ETF",3.113`
+  let content = parse.parse(jsonObj);
   fs.writeFile(csvPath, content, "utf8", function (err) {
     if (err) {
       console.log("An error occured while writing JSON Object to File.");
