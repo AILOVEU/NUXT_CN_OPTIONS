c6329e45c47b1ea6618482802b4665bd6e27688b
wanghuan <1032290763@QQ.COM>
2025-12-30 11:52:02 +0800

    123

---
+++ app/components/tag/ExchangeTag.vue
@@ -0,0 +1,12 @@
+<template>
+    <el-tag type="info" size="small" effect="plain">
+      <div :style="{ color: 涨跌 > 0 ? 'red' : 'green' }">
+       
+      </div>
+    </el-tag>
+  </template>
+  <script setup>
+  const props = defineProps(["涨跌"]);
+
+  </script>
+  
