b8fd6680d64536700acb9dd84b0fa10010ebb732
wanghuan <1032290763@QQ.COM>
2026-01-10 19:50:52 +0800

    222

--- app/pages/fund/index.vue
+++ app/pages/fund/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-md:w-[300%]">
+  <div class="max-md:w-[200%]">
     <div>
       <Nav />
       <div class="w-full pb-[12px]">
@@ -134,7 +134,7 @@ async function handleQuery() {
         // 文本样式配置
         style: {
           text: `${stockCode.value}  ${yearStr}${季度List[col]}`, // grid 标题内容
-          fontSize: isMobile.value ? 24 : 40, // 字体大小
+          fontSize: isMobile.value ? 12 : 40, // 字体大小
           fontWeight: "bold", // 字体加粗
           fill: "rgba('233,233,233,0.1')", // 字体颜色
           textAlign: "left", // 文本对齐方式（与 left 配合）
