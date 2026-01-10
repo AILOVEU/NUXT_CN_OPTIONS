2fe69d896c8f938eb8c993569927f92c7be0ba25
wanghuan <1032290763@QQ.COM>
2026-01-10 20:45:54 +0800

    222

--- app/stores/useGlobal.js
+++ app/stores/useGlobal.js
@@ -1,15 +1,9 @@
 import { defineStore } from "pinia";
-import { useMediaQuery } from "@vueuse/core";
-import UAParser from 'ua-parser-js'
+// import { useMediaQuery } from "@vueuse/core";
+// import UAParser from "ua-parser-js";
 
 export const useGlobal = defineStore("globalLoading", () => {
-  const isMobile = ref(false)
-  
-  const userAgent = navigator.userAgent
-  const parser = new UAParser(userAgent)
-  const deviceType = parser.getDevice().type
-  
-  isMobile.value = deviceType === 'mobile' || deviceType === 'tablet'
+  const isMobile = ref(false);
 
   const globalLoading = reactive({ value: false });
   const mobileScale = ref(355);
@@ -18,6 +12,16 @@ export const useGlobal = defineStore("globalLoading", () => {
     console.log("val", val);
     globalLoading.value = val;
   }
+
+  onMouted(() => {
+    // const userAgent = navigator.userAgent;
+    // const parser = new UAParser(userAgent);
+    // const deviceType = parser.getDevice().type;
+
+    // isMobile.value = deviceType === "mobile" || deviceType === "tablet";
+    isMobile.value = /Mobile|Android|iOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i.test(navigator.userAgent)
+
+  });
 
   return { globalLoading, setGlobalLoading, mobileScale, isMobile };
 });
