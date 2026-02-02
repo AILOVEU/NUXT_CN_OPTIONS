da014662bd4d312d1e8057b3c69927e85b386391
wanghuan <1032290763@QQ.COM>
2026-02-02 15:04:53 +0800

    666

--- server/api/queryEastmoney.js
+++ server/api/queryEastmoney.js
@@ -1,7 +1,7 @@
 // import dayjs from "dayjs";
 
 const COOKIE_LIST = {
-  _: "fullscreengg=1; fullscreengg2=1; qgqp_b_id=9b5c5b018a8a9ff2f8d9780997fc2519; st_nvi=QfGwF2eQsfQaV4KwwLZGX9daf; st_si=49793737665556; st_asi=delete; nid18=010d039dd427dc4d187090491f47d7ad; nid18_create_time=1769756906861; gviem=pCr0vkHrQopE8T7buOwMQa897; gviem_create_time=1769756906862; wsc_checkuser_ok=1; st_pvi=34716232359452; st_sp=2026-01-30%2015%3A08%3A26; st_inirUrl=; st_sn=5; st_psi=20260130160007977-113200301321-4654561411",
+  _: "fullscreengg=1; fullscreengg2=1; qgqp_b_id=9b5c5b018a8a9ff2f8d9780997fc2519; st_nvi=-URusFo8lKgiAjozMj5bGe97b; st_si=87931884780418; st_pvi=32076501664324; st_sp=2026-02-02%2015%3A01%3A21; st_inirUrl=; st_sn=1; st_psi=20260202150121733-113200301321-2029983405; st_asi=delete; nid18=010d039dd427dc4d187090491f47d7ad; nid18_create_time=1770015682020; gviem=xw73ZqS7v0h6ZMu-Gq9JXef51; gviem_create_time=1770015682020",
 };
 export default eventHandler(async (event) => {
   let cookie = COOKIE_LIST[getQuery(event)["fs"]] || COOKIE_LIST["_"];
