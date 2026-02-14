e5705686c54e60330ec3bc15865c903f9eb61556
wanghuan <1032290763@QQ.COM>
2026-02-14 11:46:23 +0800

    77

--- server/api/queryEastmoney.js
+++ server/api/queryEastmoney.js
@@ -1,7 +1,7 @@
 // import dayjs from "dayjs";
 
 const COOKIE_LIST = {
-  _: "fullscreengg=1; fullscreengg2=1; qgqp_b_id=9b5c5b018a8a9ff2f8d9780997fc2519; st_nvi=y7BYR8q1U2SHGwn1-GzUK7bf4; st_si=15912908314336; st_asi=delete; nid18=010d039dd427dc4d187090491f47d7ad; nid18_create_time=1770793551438; gviem=A9bdiUwg-I7EnaGd0w9Gi3808; gviem_create_time=1770793551438; wsc_checkuser_ok=1; st_pvi=05596905424376; st_sp=2026-02-11%2015%3A05%3A51; st_inirUrl=; st_sn=3; st_psi=20260211150609578-113200301321-1667067146",
+  _: "qgqp_b_id=9b5c5b018a8a9ff2f8d9780997fc2519; st_nvi=y7BYR8q1U2SHGwn1-GzUK7bf4; nid18=010d039dd427dc4d187090491f47d7ad; nid18_create_time=1770793551438; gviem=A9bdiUwg-I7EnaGd0w9Gi3808; gviem_create_time=1770793551438; EMFUND1=null; EMFUND2=null; EMFUND3=null; EMFUND4=null; EMFUND5=null; EMFUND6=null; EMFUND7=null; EMFUND8=null; EMFUND0=null; EMFUND9=02-11 16:22:07@#$%u5BCC%u56FD%u4E2D%u8BC1%u6E2F%u80A1%u901A%u4E92%u8054%u7F51ETF@%23%24159792; fullscreengg=1; fullscreengg2=1; st_si=38293442910260; st_pvi=05596905424376; st_sp=2026-02-11%2015%3A05%3A51; st_inirUrl=https%3A%2F%2Fwww.google.com.hk%2F; st_sn=1; st_psi=20260214114231859-113200301321-3443320868; st_asi=delete",
 };
 export default eventHandler(async (event) => {
   let cookie = COOKIE_LIST[getQuery(event)["fs"]] || COOKIE_LIST["_"];
