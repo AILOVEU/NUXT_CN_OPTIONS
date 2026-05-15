4a39f24eb38e198a9a9bb742ff76d3b454f36570
wanghuan <1032290763@QQ.COM>
2026-05-16 01:06:49 +0800

    111

---
+++ app/components/TagHoldDiffPercent.vue
@@ -0,0 +1,15 @@
+<template>
+  <MyTag :label="props.value > 0 ? '盈' : '亏'" :value="props.收益率" :cfg="cfg">
+    <span class="transform origin-center [zoom:0.6]">{{ props.收益率.toFixed(0) }}%</span>
+    <div class="inline-block">{{ props.value }}</div>
+  </MyTag>
+</template>
+<script setup>
+const props = defineProps(["value", "收益率"]);
+const cfg = [
+  [-999999, -50, "bg-green"],
+  [-50, 0, "green"],
+  [0, 100, "red"],
+  [100, 999999, "bg-red"],
+];
+</script>
