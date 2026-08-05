1b92c548ffbf92c9de5186f8e402b3293b969b91
wanghuan <15428692+ailoveuhy@user.noreply.gitee.com>
2026-08-05 22:28:45 +0800

    666

--- server/api/query_Eastmoney_StockIndex.js
+++ server/api/query_Eastmoney_StockIndex.js
@@ -1,7 +1,7 @@
 // import dayjs from "dayjs";
 
 const COOKIE_LIST = {
-  _: "qgqp_b_id=fb5df5b6e0a7997070f2db46d13f174b; st_nvi=L1yWEeMo0BhKl3nWNh5Tb07e2; nid18=0f4f961de20f18db67adee4fe5e66cff; nid18_create_time=1785402357622; gviem=xvoxpNTEUNy_P-et58JHJ1c92; gviem_create_time=1785402357622; st_si=27037550150532; st_pvi=85216879439588; st_sp=2026-07-30%2017%3A05%3A56; st_inirUrl=https%3A%2F%2Fwww.google.com.hk%2F; st_sn=1; st_psi=20260804150400859-113200301321-4929574315; st_asi=delete",
+  _: "qgqp_b_id=fb5df5b6e0a7997070f2db46d13f174b; st_nvi=DrSjGaplkwRgStWrxhuFN63a6; st_si=23195997070155; st_pvi=89950240211233; st_sp=2026-08-05%2022%3A23%3A18; st_inirUrl=; st_sn=1; st_psi=20260805222318578-113200301321-4659853510; st_asi=delete; nid18=004e74e9232417c75418fe255a1df0bb; nid18_create_time=1785939799589; gviem=4ns1Z0YySI2u6NRKZ8lmeab2c; gviem_create_time=1785939799589",
 };
 export default eventHandler(async (event) => {
   let cookie = COOKIE_LIST[getQuery(event)["fs"]] || COOKIE_LIST["_"];
